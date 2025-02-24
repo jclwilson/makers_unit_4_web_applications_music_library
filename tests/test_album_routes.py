b1dbc86536f5983dcd3f431d442ee8f6d5758e71
Jacob Wilson <hello@jacobcharleswilson.com>
2025-02-24 17:10:39 +0000

    Implements test for album route

---
+++ tests/test_album_routes.py
@@ -0,0 +1,18 @@
+'''
+Test file for album routes
+'''
+
+def test_get_albums_from_database(web_client, db_connection) -> None:
+    '''
+    Tests that when we call GET on /albums endpoint then all albums are returned
+    '''
+    db_connection.seed('seeds/music_library.sql')
+    response = web_client.get('/albums')
+    assert response.status_code == 200
+    assert response.data.decode("utf-8") == '''["Doolittle (1989)","Surfer Rosa (1988)","Waterloo (1974)","Super Trouper (1980)","Bossanova (1990)","Lover (2019)","Folklore (2020)","I Put a Spell on You (1965)","Baltimore (1978)","Here Comes the Sun (1971)","Fodder on My Wings (1982)","Ring Ring (1973)"]\n'''
+
+def test_album_is_added_to_database(web_client) -> None:
+    '''
+    Test that when we call POST on the albums endpoint, the information is added to the database.
+    '''
+    pass
