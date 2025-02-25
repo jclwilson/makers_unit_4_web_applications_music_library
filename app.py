40c5d8461abf885fcbdf648fa596d52b9da480b9
Jacob Wilson <hello@jacobcharleswilson.com>
2025-02-25 11:36:21 +0000

    Implements test and route for adding albums

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import os
 
 from lib.database_connection import get_flask_database_connection
 from lib.album_repository import AlbumRepository
+from lib.album import Album
 
 from flask import Flask, request
 
@@ -21,6 +22,20 @@ def get_all_albums():
     album_repository = AlbumRepository(connection)
     albums = album_repository.all()
     return [f"{album.title} ({album.release_year})" for album in albums]
+
+@app.route('/albums', methods=['POST'])
+def add_album():
+    '''
+    Adds an album to the database via a POST request
+    '''
+    connection = get_flask_database_connection(app)
+    album_repository = AlbumRepository(connection)
+    album_title: str = request.form['title']
+    album_release_year: str = request.form['release_year']
+    album_artist_id: str = request.form['artist_id']
+    new_album: Album = Album(None, album_title, album_release_year, album_artist_id)
+    album_repository.create(new_album)
+    return ''
 
 
 if __name__ == '__main__':
--- tests/test_album_routes.py
+++ tests/test_album_routes.py
@@ -23,4 +23,11 @@ def test_album_is_added_to_database(web_client, db_connection) -> None:
     '''
     Test that when we call POST on the albums endpoint, the information is added to the database.
     '''
-    pass
+    db_connection.seed('seeds/music_library.sql')
+    album_data: dict = {'title': 'National Anthem', 'release_year': 2000, 'artist_id': 1}
+    response = web_client.post('/albums', data = album_data)
+    assert response.status_code == 200
+    assert response.data.decode('utf-8') == ""
+    response_2 = web_client.get('/albums')
+    assert response_2.status_code == 200
+    assert response_2.data.decode("utf-8") == '''["Doolittle (1989)","Surfer Rosa (1988)","Waterloo (1974)","Super Trouper (1980)","Bossanova (1990)","Lover (2019)","Folklore (2020)","I Put a Spell on You (1965)","Baltimore (1978)","Here Comes the Sun (1971)","Fodder on My Wings (1982)","Ring Ring (1973)","National Anthem (2000)"]\n'''
