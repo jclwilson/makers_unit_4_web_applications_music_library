d2f1d12fde63424e4df6fb23d6d896416f1dbd52
Jacob Wilson <hello@jacobcharleswilson.com>
2025-02-25 11:34:59 +0000

    Adds test for 400 and rewrites get albums route

--- app.py
+++ app.py
@@ -13,18 +13,16 @@ from flask import Flask, request
 app: Flask = Flask(__name__)
 
 @app.route('/albums', methods=['GET'])
-def get_all_albums() -> str:
+def get_all_albums():
     '''
     Returns all albums to the browser.
-    :param artist_repository: The object holding the database connection.
     '''
     connection = get_flask_database_connection(app)
     album_repository = AlbumRepository(connection)
     albums = album_repository.all()
-    results = []
-    for album in albums:
-        results.append(f"{album.title} ({album.release_year})")
-    return results
+    return [f"{album.title} ({album.release_year})" for album in albums]
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=int(os.environ.get('PORT', 5001)))
+
--- tests/test_album_routes.py
+++ tests/test_album_routes.py
@@ -11,7 +11,15 @@ def test_get_albums_from_database(web_client, db_connection) -> None:
     assert response.status_code == 200
     assert response.data.decode("utf-8") == '''["Doolittle (1989)","Surfer Rosa (1988)","Waterloo (1974)","Super Trouper (1980)","Bossanova (1990)","Lover (2019)","Folklore (2020)","I Put a Spell on You (1965)","Baltimore (1978)","Here Comes the Sun (1971)","Fodder on My Wings (1982)","Ring Ring (1973)"]\n'''
 
-def test_album_is_added_to_database(web_client) -> None:
+def test_post_albums_from_database(web_client, db_connection) -> None:
+    '''
+    Test that we recieve a 400 BAD REQUEST on POST requests.
+    '''
+    db_connection.seed('seeds/music_library.sql')
+    response = web_client.post('/albums')
+    assert response.status_code == 400
+
+def test_album_is_added_to_database(web_client, db_connection) -> None:
     '''
     Test that when we call POST on the albums endpoint, the information is added to the database.
     '''
