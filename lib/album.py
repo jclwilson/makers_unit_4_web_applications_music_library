6bd89135206bd5c6f886de27ade48340888ef580
Jacob Wilson <hello@jacobcharleswilson.com>
2025-02-27 13:32:44 +0000

    Implements Album class validation method

--- lib/album.py
+++ lib/album.py
@@ -14,3 +14,13 @@ class Album:
 
     def __repr__(self) -> str:
         return f"Album({self.id}, {self.title}, {self.release_year}, {self.artist_id})"
+
+    def is_valid(self) -> bool:
+        """Check validity of submitted data."""
+        if isinstance(self.title, str) \
+                and len(self.title) > 0 \
+                and isinstance(self.release_year, int) \
+                and isinstance(self.artist_id, int):
+            return True
+        return False
+
