7f4d8648568bc53cc1ecdd66615e29e7c861bd48
Jacob Wilson <hello@jacobcharleswilson.com>
2025-02-27 13:33:31 +0000

    Implements album class generate error method

--- lib/album.py
+++ lib/album.py
@@ -24,3 +24,11 @@ class Album:
             return True
         return False
 
+    def generate_errors(self) -> list:
+        """Return human-readable errors if data is invalid.""" 
+        errors = []
+        if self.title is None or self.title == "":
+            errors.append("Album title cannot be blank")
+        if self.release_year is None or isinstance(self.release_year, int) == False:
+            errors.append("Release year must be a number")
+        return errors
